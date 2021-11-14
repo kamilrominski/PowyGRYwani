003b3d7f4ab939b6f5ba2f4bc94b606679214bc7
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2021-11-14 11:53:10 +0100

    User edit

--- front/src/views/UserProfileEdit.vue
+++ front/src/views/UserProfileEdit.vue
@@ -19,34 +19,15 @@
         <div class="col-12">
           <card shadow type="secondary">
             <form>
-              <h6 class="heading-small text-muted mb-4">User information</h6>
+              <h6 class="heading-small text-muted mb-4">
+                Informacje o użytkowniku
+              </h6>
               <div class="pl-lg-4">
                 <div class="row">
                   <div class="col-lg-6">
                     <base-input
                       alternative=""
-                      label="Nazwa użytkownika"
-                      placeholder="Username"
-                      input-classes="form-control-alternative"
-                      v-model="model.username"
-                    />
-                  </div>
-                  <div class="col-lg-6">
-                    <base-input
-                      alternative=""
-                      label="Email address"
-                      placeholder="jesse@example.com"
-                      input-classes="form-control-alternative"
-                      v-model="model.email"
-                    />
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-lg-6">
-                    <base-input
-                      alternative=""
-                      label="First name"
-                      placeholder="First name"
+                      label="Imię"
                       input-classes="form-control-alternative"
                       v-model="model.firstName"
                     />
@@ -54,14 +35,24 @@
                   <div class="col-lg-6">
                     <base-input
                       alternative=""
-                      label="Last name"
-                      placeholder="Last name"
+                      label="Nazwisko"
                       input-classes="form-control-alternative"
                       v-model="model.lastName"
                     />
                   </div>
                 </div>
-                <a href="#!" class="btn btn-info">Edit profile</a>
+
+                <div class="row">
+                  <div class="col-lg-6">
+                    <base-input
+                      alternative=""
+                      label="Email"
+                      input-classes="form-control-alternative"
+                      v-model="model.email"
+                    />
+                  </div>
+                </div>
+                <a href="#!" class="btn btn-info">Edytuj profil</a>
               </div>
             </form>
           </card>
