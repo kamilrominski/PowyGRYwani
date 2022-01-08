c3991880cbf0406777a765bb439887e7f484d4ba
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2022-01-08 14:02:45 +0100

    Description fix

--- front/src/views/StudioEdit.vue
+++ front/src/views/StudioEdit.vue
@@ -19,9 +19,7 @@
         <div class="col-12">
           <card shadow type="secondary">
             <form>
-              <h6 class="heading-small text-muted mb-4">
-                Informacje o użytkowniku
-              </h6>
+              <h6 class="heading-small text-muted mb-4">Informacje o studiu</h6>
               <div class="pl-lg-4">
                 <div class="row">
                   <div class="col-lg-6">
