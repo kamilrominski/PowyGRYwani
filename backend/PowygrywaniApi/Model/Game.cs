53495695a1fcd00cd7e06d6d40d43bcbc841e68a
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2022-01-22 17:16:50 +0100

    Name validation

--- backend/PowygrywaniApi/Model/Game.cs
+++ backend/PowygrywaniApi/Model/Game.cs
@@ -9,7 +9,7 @@ namespace PowygrywaniApi.Model
     public class Game : BaseModelTemplate
     {
         public string? Description { get; set; }
-        public int Studio_id { get; set; }
-        public int Series_id { get; set; }
+        public int? Studio_id { get; set; }
+        public int? Series_id { get; set; }
     }
 }
--- backend/PowygrywaniApi/Model/Templates/BaseModelTemplate.cs
+++ backend/PowygrywaniApi/Model/Templates/BaseModelTemplate.cs
@@ -2,7 +2,7 @@
 {
     public class BaseModelTemplate : ModelIdTemplate
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Type { get; set; }
     }
 }
